56e786272c40d77b69ba52b6fa0752487b71aaf9
lukas255 <lukas255@o2.pl>
2016-03-03 13:58:07 +0100

    structure

---
+++ src/main/java/com/test/MainApp.java
@@ -0,0 +1,22 @@
+package com.test;
+
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        /*ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TaskDispatcher dispatcher1 = (TaskDispatcher) context.getBean("taskDispatcher");
+        dispatcher1.executeAllTasks();
+
+        dispatcher1.setDispatcherName("dispatcher 1");
+        System.out.println("Dispatcher1 name: " + dispatcher1.getDispatcherName());
+
+        TaskDispatcher dispatcher2 = (TaskDispatcher) context.getBean("taskDispatcher");
+        dispatcher2.executeAllTasks();
+
+        System.out.println("Dispatcher2 name: " + dispatcher2.getDispatcherName());*/
+    }
+}
